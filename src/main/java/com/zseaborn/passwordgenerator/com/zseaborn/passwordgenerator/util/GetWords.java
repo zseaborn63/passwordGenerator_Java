fd36e500e9dfad3f4ddb55689dc42623436691cf
Zach Seaborn <zach@seaborn.dev>
2026-04-01 10:02:46 -0400

    Add basic GetWords util

---
+++ src/main/java/com/zseaborn/passwordgenerator/com/zseaborn/passwordgenerator/util/GetWords.java
@@ -0,0 +1,11 @@
+package com.zseaborn.passwordgenerator.util;
+public class GetWords {
+
+    public static String[] main(){
+        String[] retVal;
+        retVal = new String[] {"test", "Ball", "four"};
+        return retVal;
+        
+    }
+    
+}
